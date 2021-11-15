eef5888f30b9f9d6544e456d8cc37a208f50612d
Jorge Rodríguez <rodriguez.jorged@gmail.com>
2021-11-15 18:59:01 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -2,15 +2,49 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
+class Bienvenida(BaseEstimator, TransformerMixin):
+    def __init__(self):
+        pass
+
+    def fit(self, df,Y=None):
+        return df
+
+    def transform(self,df):
+        # Copia del Dataframe enviado
+        dfcop = df.copy()
+        return dfcop
+
+#Transformar para One-Hot Encoding para Variables Categóricas
+class Labeling(BaseEstimator, TransformerMixin):
+    toEncode=["SEX","EXISTING_SAVINGS","CHECKING_BALANCE","JOB_TYPE",'CREDIT_HISTORY','LOAN_PURPOSE','OTHERS_ON_LOAN','PROPERTY',
+          'INSTALLMENT_PLANS','HOUSING']
+    def __init__(self, columnas):
+        self.columnas = columnas
+        
+
+    def fit(self, df, y=None):
+        return self
+
+    def transform(self, X):
+        # Primero realizamos la cópia del DataFrame 'X' de entrada
+        data = X.copy()
+        # Retornamos um nuevo dataframe sin las colunmas indeseadas
+        df_encoded = pd.get_dummies(data, columns=self.columnas)
+        return df_encoded
+    
+
+# Un transformador para remover columnas indeseadas
+class DropColumns(BaseEstimator, TransformerMixin):    
+    columns=["ID","TELEPHONE","ALLOW"]
     def __init__(self, columns):
         self.columns = columns
 
     def fit(self, X, y=None):
-        return self
+        return X
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        # Primero realizamos la cópia del DataFrame 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        # Retornamos um nuevo dataframe sin las colunmas indeseadas
+        return data.drop(labels=self.columns, axis='columns')    
+    
