b719285a4272a48aff4f4f997447e19ab010df99
Jorge Rodríguez <rodriguez.jorged@gmail.com>
2021-11-15 19:48:33 -0500

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -16,6 +16,7 @@ class Bienvenida(BaseEstimator, TransformerMixin):
 
 #Transformar para One-Hot Encoding para Variables Categóricas
 class Labeling(BaseEstimator, TransformerMixin):
+    import pandas as pd
     toEncode=["SEX","EXISTING_SAVINGS","CHECKING_BALANCE","JOB_TYPE",'CREDIT_HISTORY','LOAN_PURPOSE','OTHERS_ON_LOAN','PROPERTY',
           'INSTALLMENT_PLANS','HOUSING']
     def __init__(self):
@@ -29,7 +30,7 @@ class Labeling(BaseEstimator, TransformerMixin):
         # Primero realizamos la cópia del DataFrame 'X' de entrada
         data = X.copy()
         # Retornamos um nuevo dataframe sin las colunmas indeseadas
-        df_encoded = pandas.get_dummies(data, columns=toEncode)
+        df_encoded = pd.get_dummies(data, columns=toEncode)
         return df_encoded
     
 
